779e9a92990b26b740a6b8f571db5b8cffef49eb
fpref <alla.g.vlasova@gmail.com>
2017-08-30 13:38:09 -0700

    style: indentation in App.js was made consistent

--- src/App.js
+++ src/App.js
@@ -8,90 +8,81 @@ import './App.css'
 
 
 class BooksApp extends React.Component {
-  state = {
-    books: [],
-    shelves: [
-      {
-        shelfName: "Currently Reading",
-        shelfValue: "currentlyReading"
-      }, 
-      {
-        shelfName: "Want to Read",
-        shelfValue: "wantToRead"
-      }, 
-      {
-        shelfName: "Read",
-        shelfValue: "read"
-      } 
-      ]           
-  }
+    state = {
+        books: [],
+        shelves: [
+            {
+                shelfName: "Currently Reading",
+                shelfValue: "currentlyReading"
+            }, 
+            {
+                shelfName: "Want to Read",
+                shelfValue: "wantToRead"
+            }, 
+            {
+                shelfName: "Read",
+                shelfValue: "read"
+            } 
+        ]           
+    }
 
 
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
-    })
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({ books: books })
+        })
+    }
 
-  }
-
-  changeShelf = (book, shelf) =>  {
+    changeShelf = (book, shelf) =>  {
     
-    const books = this.state.books;
-    const index = books.map(function(b){
-        return b.id;
-    }).indexOf(book.id);
-    if (index !== -1) {
-      books[index].shelf = shelf;
-      this.setState({books:books});
+        const books = this.state.books;
+        const index = books.map(
+            function(b){
+                return b.id;
+            }).indexOf(book.id);
+        if (index !== -1) {
+            books[index].shelf = shelf;
+            this.setState({books:books});
+        }
+        else {
+            this.setState(state => ({
+            books: state.books.concat([ book ])
+            }));   
+        }
+        BooksAPI.update(book, shelf)
+    } 
+
+    render() {
+        return (
+            <div className="app">
+                <Route exact path='/' render={() => (
+                    <div className="list-books">
+                        <div className="list-books-title">
+                            <h1>MyReads</h1>
+                        </div>
+                        <div className="list-books-content">
+                            <ListShelves
+                                books={this.state.books}
+                                shelves={this.state.shelves}
+                                onChangeBookShelf={this.changeShelf}
+                            />
+                        </div>
+                        <div className="open-search">
+                            <Link to="/search">Add a book</Link>
+                        </div>
+                    </div>
+                )}/>
+                <Route path='/search' render={({ history }) => (
+                    <div className="search-books">
+                        <Search
+                            libraryBooks={this.state.books}
+                            onChangeBookShelf={this.changeShelf}
+                        /> 
+                    </div>
+                )}/>        
+            </div>
+        )
     }
-    else {
-      this.setState(state => ({
-        books: state.books.concat([ book ])
-      }));   
-    }
-    BooksAPI.update(book, shelf)
-  } 
-
-  render() {
-    return (
-      <div className="app">
-        <Route exact path='/' render={() => (
-          <div className="list-books">
-
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-             <ListShelves
-              books={this.state.books}
-              shelves={this.state.shelves}
-              onChangeBookShelf={this.changeShelf}
-            />
-              <div>
-
-
-
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
-        )}/>
-        <Route path='/search' render={({ history }) => (
-         
-          <div className="search-books">
-            <Search
-              libraryBooks={this.state.books}
-              onChangeBookShelf={this.changeShelf}
-            /> 
-          </div>
-         )}/>        
-
-
-      </div>
-    )
-  }
 }
 
 export default BooksApp
