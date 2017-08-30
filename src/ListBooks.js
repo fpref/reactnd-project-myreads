eadfad8744820555e4b7c0132b6489fc90742386
fpref <alla.g.vlasova@gmail.com>
2017-08-30 13:18:06 -0700

    style: indentation made consistent, 4 spaces

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,56 +2,46 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
-  }
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        onChangeBookShelf: PropTypes.func.isRequired
+    }
 
+    render() {
+        const {books, onChangeBookShelf} = this.props
 
- 
-
-  render() {
-  	const {books, onChangeBookShelf} = this.props
-
-
-
-
-    return (
-
-                  
-        <ol className="books-grid">
-          {books.map((book) => (
-                      <li key={book.id}>
+        return (                  
+            <ol className="books-grid">
+                {books.map((book) => (
+                    <li key={book.id}>
                         <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ 
-                              width: 128, 
-                              height: 193, 
-                              backgroundSize  : '128px 193px', 
-                              backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : "https://books.google.com/googlebooks/images/no_cover_thumb.gif"})` 
-                              }}
-                            >
+                            <div className="book-top">
+                                <div className="book-cover" style={{ 
+                                  width: 128, 
+                                  height: 193, 
+                                  backgroundSize  : '128px 193px', 
+                                  backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : "https://books.google.com/googlebooks/images/no_cover_thumb.gif"})` 
+                                  }}
+                                >
+                                </div>
+                                <div className="book-shelf-changer">
+                                    <select value={(book.shelf) ? book.shelf : "none"} onChange={(event) => onChangeBookShelf(book, event.target.value)}>
+                                        <option value="none1" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
                             </div>
-                            <div className="book-shelf-changer">
-                              <select value={(book.shelf) ? book.shelf : "none"} onChange={(event) => onChangeBookShelf(book, event.target.value)}>
-                                <option value="none1" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          {book.authors && book.authors.map((author) =>(<div className="book-authors" key={author}>{author}</div>))}
-                          
+                            <div className="book-title">{book.title}</div>
+                            {book.authors && book.authors.map((author) =>(<div className="book-authors" key={author}>{author}</div>))}  
                         </div>
-                      </li>
-          ))}
-        </ol>
-
-    )
-  }
+                    </li>
+                ))}
+            </ol>
+        )
+    }
 }
 
 export default ListBooks
--- src/Search.js
+++ src/Search.js
@@ -5,75 +5,74 @@ import ListBooks from './ListBooks'
 import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
-  static propTypes = {
-    libraryBooks: PropTypes.array.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
-  }
+    static propTypes = {
+        libraryBooks: PropTypes.array.isRequired,
+        onChangeBookShelf: PropTypes.func.isRequired
+    }
 
-  state = {
-    foundBooks: []
-  }
+    state = {
+        foundBooks: []
+    }
 
-  updateQuery = (query) => {
-    query = query.trim();
-    if (query) {
-      BooksAPI.search(query, 10).then(data => {
-          if (data && data.length > 0) {
-            const bookIds = data.map(b => b.id);
-            this.props.libraryBooks.forEach(function(libraryBook) {
-                const index = bookIds.indexOf(libraryBook.id);
-                if (index !== -1) {
-                    data[index].shelf = libraryBook.shelf;
+    updateQuery = (query) => {
+        query = query.trim();
+        if (query) {
+            BooksAPI.search(query, 10).then(data => {
+                if (data && data.length > 0) {
+                    const bookIds = data.map(b => b.id);
+                    this.props.libraryBooks.forEach(
+                        function(libraryBook) {
+                            const index = bookIds.indexOf(libraryBook.id);
+                            if (index !== -1) {
+                            data[index].shelf = libraryBook.shelf;
+                            }
+                        }
+                    )
+                    this.setState({foundBooks: data})            
                 }
-            })
-            this.setState({foundBooks: data})            
-          }
-
-      }
-      )    
-    }
-    else {
-      this.setState({foundBooks:[]})
-    }
-  }
+            })    
+        }
+        else {
+            this.setState({foundBooks:[]})
+        }
+    } 
  
-  changeShelfInSearch = (book, shelf) =>  {
+    changeShelfInSearch = (book, shelf) =>  {
     
-    const books = this.state.foundBooks;
-    const index = books.map(function(b){
-        return b.id;
-    }).indexOf(book.id);
-    if (index !== -1) {
-      books[index].shelf = shelf;
-      this.setState({foundBooks:books});
-      this.props.onChangeBookShelf(book, shelf);
-    }
-  } 
+        const books = this.state.foundBooks;
+        const index = books.map(
+            function(b){
+                return b.id;
+            }).indexOf(book.id);
+        if (index !== -1) {
+            books[index].shelf = shelf;
+            this.setState({foundBooks:books});
+            this.props.onChangeBookShelf(book, shelf);
+        }
+    } 
 
-  render() {   
-    const {foundBooks} = this.state
+    render() {   
+        const {foundBooks} = this.state
   
-
-    return (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"
-                  
-                  onChange={(event) => this.updateQuery(event.target.value)}
-                />                
-              </div>
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author"
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                        />                
+                    </div>
+                </div>
+                {foundBooks && foundBooks.length > 0 && (<div className="search-books-results">
+                    <ListBooks
+                        books={foundBooks}
+                        onChangeBookShelf={this.changeShelfInSearch}
+                    />
+                </div>)}
             </div>
-            {foundBooks && foundBooks.length > 0 && (<div className="search-books-results">
-              <ListBooks
-                books={foundBooks}
-                onChangeBookShelf={this.changeShelfInSearch}
-              />
-            </div>)}
-          </div>
-    )
-  }
+        )
+    }
 }
 
 export default Search
